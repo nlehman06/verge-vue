03bbfb3c51d3ca4a5114efbcd41a1edba406033b
nlehman06 <nlehman06@gmail.com>
2018-10-04 22:46:06 -0400

    try without purgecss

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,11 +3,11 @@ const isDev = process.argv.indexOf("serve") !== -1;
 module.exports = {
   plugins: [
     require("tailwindcss")("./src/tailwind/tailwind.js"),
-    isDev
+    /*isDev
       ? ""
       : purgecss({
-          content: ["./src/**/*.html", "./src/!**!/!*.vue", "./src/!**/!*.scss"]
-        }),
+          content: ["./src/!**!/!*.html", "./src/!**!/!*.vue", "./src/!**!/!*.scss"]
+        }),*/
     require("autoprefixer")
   ]
 };
